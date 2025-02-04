4cfd2e95eed48bb0435f80290698c6d1be761f29
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-02-04 14:01:17 -0600

    Not found

--- src/app.js
+++ src/app.js
@@ -6,5 +6,9 @@ const app = express();
 app.use(express.json());
 app.use('/api', router);
 
+app.use((req, res) => {
+    res.status(404).json({ message: 'Not found' });
+});
+
 app.listen(3000);
 console.log("Server on port", 3000);
